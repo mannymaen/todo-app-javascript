ee110d67b2b9191637a55d4a15d000b1a763e50a
Emanuel Ledesma <34849198+mannymaen@users.noreply.github.com>
2021-04-24 14:13:15 -0500

    FEATURE delete task finished.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ let addTaskBtn = document.getElementById('add-task-btn');
 let tasksList = document.getElementsByClassName('list-group')[0];
 let tasks = [];
 
+let getRandomId = () => {
+    return Math.floor(Math.random() * 999999);
+}
+
 let toggleDisableAddTaskBtn = () => {
     addTaskBtn.toggleAttribute('disabled', isAddTaskBtnDisabled);
 }
@@ -11,23 +15,73 @@ let toggleDisableAddTaskBtn = () => {
 let createTaskListItem = (task) => {
 
     let li = document.createElement('li');
-    li.classList.add('list-group-item', 'pl-0');
+    li.classList.add('list-group-item', 'px-0');
 
-    let div = document.createElement('div');
-    div.classList.add('form-group', 'form-check', 'mb-0');
+    let divInputGroup = document.createElement('div');
+    divInputGroup.classList.add('input-group');
 
-    let input = document.createElement('input');
-    input.classList.add('form-check-input');
-    input.setAttribute('type', 'checkbox');
+    let divInputGroupPrepend = document.createElement('div');
+    divInputGroupPrepend.classList.add('input-group-prepend');
 
-    let label = document.createElement('label');
-    label.classList.add('form-check-label');
-    label.innerText = task;
+    let divInputGroupAppend = document.createElement('div');
+    divInputGroupAppend.classList.add('input-group-append');
 
-    div.appendChild(input);
-    div.appendChild(label);
-    li.appendChild(div);
+    let divInputGroupText = document.createElement('div');
+    divInputGroupText.classList.add('input-group-text');
+
+    let inputCheckbox = document.createElement('input');
+    inputCheckbox.setAttribute('type', 'checkbox');
+    inputCheckbox.setAttribute('name', task.id);
+    inputCheckbox.checked = task.completed;
+
+    let inputTaskText = document.createElement('input');
+    inputTaskText.classList.add('form-control');
+    inputTaskText.setAttribute('type', 'text');
+    inputTaskText.toggleAttribute('disabled');
+    inputTaskText.name = task.id;
+    inputTaskText.value = task.name;
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn', 'btn-danger');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.setAttribute('name', task.id);
+    deleteBtn.innerText = 'X'
+
+    // let label = document.createElement('label');
+    // label.classList.add('form-check-label');
+    // label.innerText = task.name;
+    divInputGroupText.appendChild(inputCheckbox);
+    divInputGroupPrepend.appendChild(divInputGroupText);
+    divInputGroupAppend.appendChild(deleteBtn);
+
+
+
+    // div.appendChild(input);
+    // div.appendChild(label);
+    li.appendChild(divInputGroup);
+    divInputGroup.append(divInputGroupPrepend);
+    divInputGroup.append(inputTaskText);
+    if (task.completed) {
+        divInputGroup.append(divInputGroupAppend);
+    }
     tasksList.appendChild(li);
+    // let div = document.createElement('div');
+    // div.classList.add('form-group', 'form-check', 'mb-0');
+
+    // let input = document.createElement('input');
+    // input.classList.add('form-check-input');
+    // input.setAttribute('type', 'checkbox');
+    // input.setAttribute('name', task.id);
+    // input.checked = task.completed;
+
+    // let label = document.createElement('label');
+    // label.classList.add('form-check-label');
+    // label.innerText = task.name;
+
+    // div.appendChild(input);
+    // div.appendChild(label);
+    // li.appendChild(div);
+    // tasksList.appendChild(li);
 }
 
 let clearChildren = (parent) => {
@@ -38,9 +92,45 @@ let clearChildren = (parent) => {
 
 let updateTasksView = () => {
     tasks.forEach(task => {
-        createTaskListItem(task.name);
+        createTaskListItem(task);
+    });
+
+    setEventListenerToTasks();
+}
+
+let setEventListenerToTasks = () => {
+    let taskCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    taskCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (event) => {
+            updateTasks(event);
+        });
+    });
+
+    let taskButtons = document.querySelectorAll('ul > li > div.input-group > div.input-group-append > button');
+    taskButtons.forEach(taskbBtn => {
+        taskbBtn.addEventListener('click', (event) => {
+            updateTasks(event, true);
+        });
     });
 }
+
+let updateTasks = (taskEvent, deleteTask = false) => {
+    let taskIndexFound = tasks.findIndex(elTask => elTask.id === taskEvent.target.name)
+    let taskFound = tasks.find(elTask => elTask.id === taskEvent.target.name);
+    if (deleteTask) {
+        tasks.splice(taskIndexFound, 1);
+    } else {
+        taskFound.completed = taskEvent.target.checked;
+        tasks.splice(
+            taskIndexFound, 
+            1, 
+            taskFound
+        );
+    }
+    clearChildren(tasksList);
+    updateTasksView();
+}
+
 
 let init = () => {
     updateTasksView(tasks);
@@ -48,7 +138,9 @@ let init = () => {
 
 addTaskBtn.addEventListener('click', (event) => {
     tasks.push({
-        name: taskInput.value
+        id: 'task'+ getRandomId(),
+        name: taskInput.value,
+        completed: false
     });
     clearChildren(tasksList);
     updateTasksView();
@@ -58,7 +150,6 @@ addTaskBtn.addEventListener('click', (event) => {
 });
 
 taskInput.addEventListener('input', (event) => {
-    console.log('event');
     isAddTaskBtnDisabled = event.target.value.length === 0;
     toggleDisableAddTaskBtn();
 });
